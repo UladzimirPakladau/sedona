b51a4b1d4f51f584565c5dac0ea6b025f63948ba
Vladimir Pokladov <122304555+UladzimirPakladau@users.noreply.github.com>
2023-04-25 13:50:24 +0300

    Добавляет стили к категории, к состоянию элементов

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,4 @@
-let popup = document.querySelector('.delivery-popup');
+let popup = document.querySelector('.search-popup');
 let popupButton = document.querySelector('.open-popup');
 let closeButton = document.querySelector('.close-popup');
 
@@ -11,20 +11,4 @@ closeButton.onclick = function() {
 };
 
 
-
-// let catalogopen = document.querySelector('.catalog-navigation .navigation-link');
-// let sublist = document.querySelector('.sub-list');
-
-// catalogopen.onclick = function() {
-//  sublist.classList.toggle('sub-list-toogle');
-// };
-
-
-
-let cartopen = document.querySelector('.cart-link');
-let cartpopup = document.querySelector('.user-cart-box-popup');
-
-cartopen.onclick = function() {
-  cartpopup.classList.toggle('popupflex');
-};
 
